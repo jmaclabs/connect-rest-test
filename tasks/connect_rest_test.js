5490f448b18b996745a3b5b2dea04cd1065c9c61
imrefazekas <imre.fazekas@gmail.com>
2013-06-24 15:16:16 +0200

    first release

--- tasks/connect_rest_test.js
+++ tasks/connect_rest_test.js
@@ -8,43 +8,83 @@
 
 'use strict';
 
+var httphelper = require('./http-helper');
+var _ = require('underscore');
+
+function doneCheck(done, counter){
+	if( counter === 0 ){
+		done();
+	}
+}
+
+function checks(name, objects, expectation, log){
+	if( (expectation === '$exists' && !_.has(objects, name)) ||
+		(expectation === '$notexists' && _.has(objects, name)) ||
+		(expectation !== '$exists' && expectation !== '$notexists' && !_.isEqual(objects[name], expectation))
+	){
+		log.error( new Error( name + ' fails to comply your expectation: ' + expectation) );
+	}
+}
+
+function check(name, object, expectation, log){
+	if( (expectation === '$exists' && !object) ||
+		(expectation === '$notexists' && object) ||
+		(!_.isEqual(object, expectation))
+	){
+		log.error( new Error( name + ' fails to comply your expectation: ' + expectation) );
+	}
+}
+
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+	grunt.registerMultiTask('connect_rest_test', 'Grunt plugin for testing REST services provided by connect-rest.', function() {
+		var options = this.options( );
 
-  grunt.registerMultiTask('connect_rest_test', 'Your task description goes here.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+		var done = this.async();
+		var threads = 0;
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+		_.each( options, function(config, key, list){
+			grunt.log.writeln( 'Processing job: ' + key + ' with config: ' + JSON.stringify(config) );
 
-      // Handle options.
-      src += options.punctuation;
+			if( !config.url || !config.method ){
+				grunt.log.error( new Error('url and method need to be set.') );
+			}
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+			threads++;
+			var timestamp = Date.now();
+			httphelper.generalCall( config.url, config.method, config.headers, null, config.payload, grunt.log,
+				function(err, result, status){
+					if(err){
+						grunt.log.error( err );
+					}
+					else{
+						if( config.expectation ){
+							var responseTime = Date.now()-timestamp;
+							if( config.expectation.responseTimeLimit && (responseTime>config.expectation.responseTimeLimit) ){
+								grunt.log.error( new Error('Response received beyond the given timeframe: ' + responseTime) );
+							}
+							if( config.expectation.statusCode && config.expectation.statusCode !== status.statusCode ){
+								grunt.log.error( new Error('Status code is not what you expected: ' + status.statusCode) );
+							}
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
+							if( config.expectation.payload ){
+								check('payload', result, config.expectation.payload, grunt.log);
+							}
+
+							var specialExps = _.filter( _.keys( config.expectation ), function(exp){ return exp.substring(0,1) === '#'; } );
+							_.each( specialExps, function(special, index, list){
+								var specialExp = config.expectation[ special ];
+
+								checks( special.substring(1,special.length), status.headers, specialExp, grunt.log);
+							});
+						}
+					}
+
+					grunt.log.writeln( 'Job finished: ' + key + ' with:' + JSON.stringify(status) );
+					doneCheck(done, threads--);
+				}
+			);
+		} );
+	});
 
 };
